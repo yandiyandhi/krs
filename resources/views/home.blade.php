479111b691c4c0e1e2c7c8bf3041003354f03573
yandiyandhi <yandiyandhi1294@gmail.com>
2023-04-09 02:10:28 +0700

    Merge branch 'master' of https://github.com/yandiyandhi/krs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,33 @@
 @extends('mainlayout.main')
-<<<<<<< HEAD
 @section('container')
     <div class="container-fluid">
         <div class="container">
             <img src="/image/BrandLogo.jpg" alt="">
-=======
-@section('content')
-    <div class="home">
-        <div class="content">
-            <div class="container">
-                {{-- <img src="/image/BrandLogo.jpg" alt=""> --}}
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, possimus, minima corporis voluptate, 
-                    vero saepe sequi laborum dolorum aut provident nesciunt itaque iure ipsam accusantium atque officia! 
-                    Totam, quibusdam repellat harum vero quo dicta maiores cupiditate temporibus rem? Tempore alias s
-                    oluta necessitatibus nisi laboriosam architecto, dolore pariatur provident reiciendis rem dignissimos 
-                    velit nulla iure, animi earum quibusdam. Magnam veniam deleniti itaque debitis delectus nesciunt similique
-                     id, qui repellendus ipsam eaque ut quidem soluta in rerum quibusdam excepturi explicabo sequi iste est 
-                     unde perspiciatis non? Perspiciatis dignissimos commodi perferendis, provident ut assumenda asperiores 
-                     sed, nostrum repellat quisquam cumque deleniti nulla quod.</p>
-            </div>
+        @section('content')
+            <div class="home">
+                <div class="content">
+                    <div class="container">
+                        {{-- <img src="/image/BrandLogo.jpg" alt=""> --}}
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, possimus, minima corporis
+                            voluptate,
+                            vero saepe sequi laborum dolorum aut provident nesciunt itaque iure ipsam accusantium atque
+                            officia!
+                            Totam, quibusdam repellat harum vero quo dicta maiores cupiditate temporibus rem? Tempore alias
+                            s
+                            oluta necessitatibus nisi laboriosam architecto, dolore pariatur provident reiciendis rem
+                            dignissimos
+                            velit nulla iure, animi earum quibusdam. Magnam veniam deleniti itaque debitis delectus nesciunt
+                            similique
+                            id, qui repellendus ipsam eaque ut quidem soluta in rerum quibusdam excepturi explicabo sequi
+                            iste est
+                            unde perspiciatis non? Perspiciatis dignissimos commodi perferendis, provident ut assumenda
+                            asperiores
+                            sed, nostrum repellat quisquam cumque deleniti nulla quod.</p>
+                    </div>
 
->>>>>>> a428698be742a5b05a4f6e9c6c9da1a56f282af4
-        </div>
-        <div class="row border content">
-            {{-- <div class="border-bottom">
+                </div>
+                <div class="row border content">
+                    {{-- <div class="border-bottom">
                 <div style="background: #2188ce; width: 9%;">
                     <div class="d-flex flex-row mb-3 text-light">
                         <div class="p-2"><i class="bi bi-gear fs-5"></i>
@@ -40,85 +44,91 @@
                     <div class="col">
                         <div class="p-3">Custom column padding</div>
                     </div>
+                </div> --}}
                 </div>
-            </div> --}}
-            <div class="row gx-2 d-flex justify-content-start align-items-start service">
-                <div class="col-8">
-                    <div class="p-3">
-                        <div class="border-bottom">
-                            <div class="services" style="background: #2188ce; width: 20%;">
-                                <div class="d-flex flex-row mb-3 text-light">
-                                    <div class="p-2"><i class="bi bi-gear fs-5"></i>
-                                        <span style="position: absolute; margin-top: 3px; margin-left: 8px;">Service</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="px-0 text-center border-top sercont">
-                                <div class="row gx-2 d-flex justify-content-start align-items-start">
-                                    <div class="card" style="width: 18rem;">
-                                        <img src="..." class="card-img-top" alt="...">
-                                        <div class="card-body">
-                                            <h5 class="card-title">Card title</h5>
-                                            <p class="card-text">Some quick example text to build on the card title and make
-                                                up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="row gx-2 d-flex justify-content-start align-items-start service">
+                    <div class="col-8">
+                        <div class="p-3">
+                            <div class="border-bottom">
+                                <div class="services" style="background: #2188ce; width: 20%;">
+                                    <div class="d-flex flex-row mb-3 text-light">
+                                        <div class="p-2"><i class="bi bi-gear fs-5"></i>
+                                            <span
+                                                style="position: absolute; margin-top: 3px; margin-left: 8px;">Service</span>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="row gx-2 d-flex justify-content-start align-items-start mt-2">
-                                    <div class="card" style="width: 18rem;">
-                                        <img src="..." class="card-img-top" alt="...">
-                                        <div class="card-body">
-                                            <h5 class="card-title">Card title</h5>
-                                            <p class="card-text">Some quick example text to build on the card title and make
-                                                up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                                <div class="px-0 text-center border-top sercont">
+                                    <div class="row gx-2 d-flex justify-content-start align-items-start">
+                                        <div class="card" style="width: 18rem;">
+                                            <img src="..." class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Card title</h5>
+                                                <p class="card-text">Some quick example text to build on the card title
+                                                    and make
+                                                    up the bulk of the card's content.</p>
+                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row gx-2 d-flex justify-content-start align-items-start mt-2">
+                                        <div class="card" style="width: 18rem;">
+                                            <img src="..." class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                                <h5 class="card-title">Card title</h5>
+                                                <p class="card-text">Some quick example text to build on the card title
+                                                    and make
+                                                    up the bulk of the card's content.</p>
+                                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col">
-                    <div class="p-3">
-                        <div class="border-bottom">
-                            <div class="sosial" style="background: #e02c2c; width: 40%;">
-                                <div class="d-flex flex-row mb-3 text-light">
-                                    <div class="p-2"><i class="bi bi-gear fs-5"></i>
-                                        <span style="position: absolute; margin-top: 3px; margin-left: 8px;">Service</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="px-0 text-center border-top sercont">
-                                <div class="row gx-2 d-flex justify-content-start align-items-start">
-                                    <div class="col">
-                                        <div class="p-3 border">
-                                            <a href="#"><i class="bi bi-facebook fs-1"></i></a>
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="p-3 border"><a href="#"><i class="bi bi-instagram fs-1"></a></i>
-                                        </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="p-3 border"><a href=""><i class="bi bi-tiktok fs-1"></a></i>
+                    <div class="col">
+                        <div class="p-3">
+                            <div class="border-bottom">
+                                <div class="sosial" style="background: #e02c2c; width: 40%;">
+                                    <div class="d-flex flex-row mb-3 text-light">
+                                        <div class="p-2"><i class="bi bi-gear fs-5"></i>
+                                            <span
+                                                style="position: absolute; margin-top: 3px; margin-left: 8px;">Service</span>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="row gx-2 d-flex justify-content-start align-items-start mt-2">
-                                    <div class="col">
-                                        <div class="p-3 border"><a href=""><i class="bi bi-youtube fs-1"></a></i>
+                                <div class="px-0 text-center border-top sercont">
+                                    <div class="row gx-2 d-flex justify-content-start align-items-start">
+                                        <div class="col">
+                                            <div class="p-3 border">
+                                                <a href="#"><i class="bi bi-facebook fs-1"></i></a>
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <div class="p-3 border"><a href="#"><i
+                                                        class="bi bi-instagram fs-1"></a></i>
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <div class="p-3 border"><a href=""><i class="bi bi-tiktok fs-1"></a></i>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="col">
-                                        <div class="p-3 border">
-                                            <a href=""><i class="bi bi-telegram fs-1"></i></a></i>
+                                    <div class="row gx-2 d-flex justify-content-start align-items-start mt-2">
+                                        <div class="col">
+                                            <div class="p-3 border"><a href=""><i class="bi bi-youtube fs-1"></a></i>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col">
-                                        <div class="p-3 border">
-                                            <a href=""><i class="bi bi-telegram fs-1"></i></a></i>
+                                        <div class="col">
+                                            <div class="p-3 border">
+                                                <a href=""><i class="bi bi-telegram fs-1"></i></a></i>
+                                            </div>
+                                        </div>
+                                        <div class="col">
+                                            <div class="p-3 border">
+                                                <a href=""><i class="bi bi-telegram fs-1"></i></a></i>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -127,8 +137,7 @@
                     </div>
                 </div>
             </div>
-        </div>
-        {{-- <div class="border">
+            {{-- <div class="border">
             <div class="border-bottom">
                 <div class="d-flex flex-row mb-3 text-light mt-3" style="background: #2188ce; width: 9%; height: 50px;">
                     <div class="p-2"><i class="bi bi-globe fs-5"></i></i>
@@ -149,5 +158,5 @@
                 </div>
             </div>
         </div> --}}
-    </div>
-@endsection
+        </div>
+    @endsection
